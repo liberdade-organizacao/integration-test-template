641349dd8a35c4095d3ef62a6ca708971d002893
Cris Jr <crisjr@pm.me>
2020-12-15 13:49:58 -0300

    Add options for different browsers

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -2,17 +2,31 @@ from os import environ
 
 import pytest
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 
 
 @pytest.fixture()
 def environment(request):
-    # setup
     yield environ
-    # teardown
 
 
 @pytest.fixture()
 def browser(request):
-    driver = webdriver.Firefox()
+    # driver setup
+    headless = bool(environ['HEADLESS'])
+
+    if environ['BROWSER'] == 'chrome':
+        options = ChromeOptions()
+        options.headless = headless
+        driver = webdriver.Chrome(options=options)
+    else:  # firefox as default
+        options = FirefoxOptions()
+        options.headless = headless
+        driver = webdriver.Firefox(options=options)
+
+    # provide driver
     yield driver
+
+    # driver teardown
     driver.close()
